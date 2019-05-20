f4bb91393b6276589d838536074d0993f09d7788
KaiserPhemi <oluwafemi.akinwa@andela.com>
2019-05-20 19:57:51 +0100

    ft(model)
    - add models
    - add routes

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+// third-party libraries
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// routes
 const authRoute = require("./src/routes/auth");
 
 const app = express();
+dotenv.config();
+const port = process.env.PORT;
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("Connected to database.")
+);
 
 // middlewares
-app.use("/api/user/", authRoute);
+app.use(express());
+app.use(express.json());
+app.use("/api/users", authRoute);
 
-app.listen(3000, () => console.log("App running on port: 3000"));
+app.listen(port, () => console.log(`App started & running on port:${port}`));
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,31 @@
 const Router = require("express");
 const route = Router();
 
-route.post("/register", (req, res) => {
+// models
+const User = require("../model/user");
+
+route.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  try {
+    res.status(201).send({
+      message: "User saved",
+      user
+    });
+  } catch {
+    res.send({
+      message: "User not created"
+    });
+  }
+});
+
+route.get("/register", (req, res) => {
   res.send({
-    message: "We are registered"
+    message: "We retrieved all registered"
   });
 });
 
